39dd8f836b17b557fd513f39ad9be777fb91f90a
hashcode-ankit <2698ankitsharma@gmail.com>
2024-12-07 15:23:51 +0530

    chore: capitalizing name

--- titan/contrib/sites/migrations/0002_set_site_domain_and_name.py
+++ titan/contrib/sites/migrations/0002_set_site_domain_and_name.py
@@ -15,7 +15,7 @@ def update_site_forward(apps, schema_editor):
     """Set site domain and name."""
     Site = apps.get_model("sites", "Site")
     Site.objects.update_or_create(
-        id=settings.SITE_ID, defaults={"domain": "titan-tool.herokuapp.com", "name": "titan"}
+        id=settings.SITE_ID, defaults={"domain": "xtitan.com", "name": "Titan"}
     )
 
 
